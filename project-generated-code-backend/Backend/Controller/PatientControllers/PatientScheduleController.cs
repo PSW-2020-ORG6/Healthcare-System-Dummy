68bd4da2126010370dd494a4137bbdd11221a822
Luka Doric <luka.doric@uns.ac.rs>
2020-11-02 20:36:48 +0100

    Test for pr

--- project-generated-code-backend/Backend/Controller/PatientControllers/PatientScheduleController.cs
+++ project-generated-code-backend/Backend/Controller/PatientControllers/PatientScheduleController.cs
@@ -15,6 +15,7 @@ namespace Backend.Controller.PatientControllers
     public class PatientScheduleController
     {
         // Test za pipeline
+        // Test za PR
         public PatientScheduleController()
         {
             appointmentSchedulingService = new AppointmentSchedulingService(new PatientSchedulingStrategy());
